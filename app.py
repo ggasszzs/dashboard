1404687d8dc5acd598427fbfe93f47dd5ba8eef2
ggasszzs <184240032@std.ulbi.ac.id>
2026-04-24 15:40:23 +0700

    Initial commit

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 import streamlit as st
-from data import show_data
+from data import show_data, select_year, load_data, filter_data, kolom, pie_chart1
 
 # Judul Dashboard
 def judul():
     st.title("📊Dashboard Covid-19")
-    st.write("Selamat datang di dashboard interaktif untuk menganalisis data COVID-19 di Indonesia")
+    st.write("Selamat datang di dashboard interaktif untuk menganalisis data COVID-19 di Indonesia.")
 
+#Sidebar Navigasi
 st.sidebar.title("Navigasi")
 menu = st.sidebar.radio("Pilih Halaman", ["Home","Halaman Data"])
+
+#Halaman Home
 if menu == "Home":
     judul()
+    #Pilih Tahun
+    year = select_year()
+    #Load & Filter Data
+    df = load_data()
+    df_filtered = filter_data(df, year)
+    kolom(df_filtered)
+    pie_chart1(df_filtered)
+
 elif menu == "Halaman Data":
     judul()
-    show_data()
+    year = select_year()
+    #Load & Filter Data
+    df = load_data()
+    df_filtered = filter_data(df, year)
+    show_data(df_filtered)
---
+++ footer.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def footer():
+    st.markdown("---")
+    st.caption("Bagaskara Bagus Dwipantara-184240032🐾")
